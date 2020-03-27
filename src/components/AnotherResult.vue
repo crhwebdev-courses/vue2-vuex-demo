92c0a85f6b8ec7a73a4a7c852a4cc5ce1a7a9f4c
Carl Hain <crhain@gmail.com>
2020-03-27 18:42:03 -0500

    User spread operator in AnotherResult so we can add additional computed properties and use mapGetters; needed to install babel-preset-stage-2 to get spread operators to compile

--- src/components/AnotherResult.vue
+++ src/components/AnotherResult.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
-    <p>Counter is: {{ counter }}</p>
-    <p>Number of Clicks: {{ clicks }}</p>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p>Number of Clicks: {{ stringCounter }}</p>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 export default {
-  computed: mapGetters({
-    counter: "doubleCounter",
-    clicks: "stringCounter"
-  })
+  computed: {
+    ...mapGetters(["doubleCounter", "stringCounter"])
+  }
 };
 </script>
